6b04714c30736f1bac8cd6dd503a28f40cd62cab
Mariaamy <mariaamy@stud.ntnu.no>
2023-02-22 18:58:31 +0100

    API

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-from flask import Flask, jsonify
+from flask import Flask, request
 import os
+
 
 app = Flask(__name__)
 
 
+CONTACTS = [{"name": "Paul"}, {"name": "Mary"}, {"name": "John"}]
+
+
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return 'Hello Flask!'
+
+
+@app.route('/contacts')
+def get_contacts():
+    return CONTACTS
+
+
+@app.route('/contacts/<id>')
+def get_contact(id):
+    return CONTACTS[int(id)]
+
+
+@app.route('/contacts', methods=['POST'])
+def update_contact():
+    name = request.json['name']
+    contact = {"name": name}
+    CONTACTS.append(contact)
+
+    id = len(CONTACTS) - 1
+    return {'id': id}
+
+
+@app.route('/contacts/<id>', methods=['DELETE'])
+def delete_contact(id):
+    del CONTACTS[int(id)]
+    return  {'id': id}
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))  # type: ignore
